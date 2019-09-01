ce821a19791466aa52ce1703e7b226a2e5a09a0e
BrabecDev <lucasbbs@dcomp.ufs.br>
2019-09-01 17:03:25 -0300

    Estabilizando app

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function fetchComments()
+    {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/', 'CommentController@index');
 
 Route::prefix('api')->group(function () {
